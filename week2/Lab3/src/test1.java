3128d04f34125493e15a9c37bd900ffc20167bec
NopXx <97354576+NopXx@users.noreply.github.com>
2023-12-13 20:48:13 +0700

    add comment code

--- week2/Lab3/src/test1.java
+++ week2/Lab3/src/test1.java
@@ -3,9 +3,16 @@ import javax.swing.JOptionPane;
 public class test1 {
     public static void main(String[] args) {
         float c, f;
+        // ประกาศตัาแปร c, f ชนิด float
+
         c = Float.parseFloat(JOptionPane.showInputDialog("Enter Celsius: "));
+        // รับค่าจากคำสั่ง JOptionPane.showInputDialog แล้วแปลงค่าเป็น float จากคำสั่ง Float.parseFloat แล้วเก็บไว้ในตัวแปร c
+
         f = (9f / 5f) * c + 32;
+        // คำนวณผลตามสูตร แล้วเก็บไว้ในตัวแปร f
+
         JOptionPane.showMessageDialog(null, "The temperature in Fahrenheit is: " + f, "Result", 1);
+        // แสดงผล The temperature in Fahrenheit is: ข้อมูลในตัวแปร f โดยใช้คำสั่ง JOptionPane.showMessageDialog
     }
     
 }
--- week2/Lab3/src/test2.java
+++ week2/Lab3/src/test2.java
@@ -3,17 +3,28 @@ import javax.swing.JOptionPane;
 public class test2 {
     public static void main(String[] args) {
         float money, rate;
-        String message_1;
+        // ประกาศตัาแปร money, rate ชนิด float
+        String message = "";
+        // ประกาศตัาแปร message ชนิด String
 
         money = Float.parseFloat(JOptionPane.showInputDialog("Enter the amount of money: "));
+        // รับค่าจากคำสั่ง JOptionPane.showInputDialog แล้วแปลงค่าเป็น float จากคำสั่ง Float.parseFloat แล้วเก็บไว้ในตัวแปร money
 
         rate = Float.parseFloat(JOptionPane.showInputDialog("Enter the interest rate: "));
+        // รับค่าจากคำสั่ง JOptionPane.showInputDialog แล้วแปลงค่าเป็น float จากคำสั่ง Float.parseFloat แล้วเก็บไว้ในตัวแปร rate 
+        
         for (int i = 1; i <= 5; i++) {
+        // ใช้ loop for โดยประกาศตัวแปร i เท่ากับ 1 ถ้า i น้อยกว่า 5 ให้เพิ่มค่า i ครั้งละ 1
+
             money += money * rate / 100;
-            message_1 += "Year " + i + " Money : " + money + "\n";
+            // คำนวณผลตามสูตร แล้วเอาค่าที่คำนวณได้มาบอกกับค่าในตัวแปร money
+
+            message += "Year " + i + ", Money : " + String.format("%.2f", money) + "\n";
+            // เก็บค่าในตัวแปร message โดยเก็บต่อจากค่าเดิมในตัวแปร
 
         }
-        JOptionPane.showMessageDialog(null, ""+ message_1 +"", "Result", 1);
+        JOptionPane.showMessageDialog(null, message, "Result", 1);
+        // แสดงผลข้อมูลในตัวแปร message โดยใช้คำสั่ง JOptionPane.showMessageDialog
 
     }
 }
