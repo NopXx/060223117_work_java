afa3d06f5359ea083fa44242e6946490e25daf55
NopXx <khamkokaewn1@hotmail.com>
2024-02-25 16:54:21 +0700

    Update TestParcelTerry.java

--- mid v2/src/TestParcelTerry.java
+++ mid v2/src/TestParcelTerry.java
@@ -3,7 +3,6 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class TestParcelTerry {
-<<<<<<< Updated upstream
 
     public static void main(String[] args) {
         ParcelTerry p = new ParcelTerry();
@@ -12,14 +11,6 @@ public class TestParcelTerry {
         JTextField textField_type, textField_weight;
         JTextArea textArea;
         textArea = new JTextArea(2, 25);
-=======
-    public static void main(String[] args) {
-        ParcelTerry p = new ParcelTerry();
-        JLabel label;
-        JButton checkBtn;
-        JTextField textField_type, textField_weight;
-        JTextArea textArea;
->>>>>>> Stashed changes
 
         JFrame frame = new JFrame("Parcel Terry");
         Container c = new Container();
@@ -41,7 +32,6 @@ public class TestParcelTerry {
         textField_weight = new JTextField(25);
         c.add(textField_weight);
 
-<<<<<<< Updated upstream
         submitBtn = new JButton("Submit");
         // submitBtn.setEnabled(false);
         c.add(submitBtn);
@@ -61,37 +51,6 @@ public class TestParcelTerry {
                     c.add(textArea);
                 }
 
-=======
-        checkBtn = new JButton("Check Input");
-        c.add(checkBtn);
-
-        label = new JLabel("Result");
-        c.add(label);
-        textArea = new JTextArea(10, 25);
-        c.add(new JScrollPane(textArea));
-
-        checkBtn.addActionListener(new ActionListener() {
-            JButton submitBtn = new JButton("Submit");
-            public void actionPerformed(ActionEvent e) {
-                submitBtn.setVisible(false);
-                c.add(submitBtn);
-                if (textField_type.getText().isEmpty() || textField_weight.getText().isEmpty()) {
-                    JOptionPane.showMessageDialog(null, "Please enter values", "", JOptionPane.ERROR_MESSAGE);
-                } else {
-                    textArea.setText("");
-                    submitBtn = new JButton("Submit");
-                    c.add(submitBtn);
-                    submitBtn.addActionListener(new ActionListener() {
-                        public void actionPerformed(ActionEvent e) {
-                            p.Settypeservice(Integer.parseInt(textField_type.getText()));
-                            p.Setweight(Integer.parseInt(textField_weight.getText()));
-                            textArea.setText(String.valueOf(p.ParcelCalator(p.Gettypeservice(0), p.Getweight())));
-                            submitBtn.setVisible(false);
-                        }
-                    });
-                    frame.revalidate(); // Refresh the frame to display the new button
-                }
->>>>>>> Stashed changes
             }
         });
 
